1d10ca224dfd6cc4b88168705074c33a96dc4b79
Ian Miller <indmillr@gmail.com>
2025-06-16 12:25:16 -0500

    three

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,7 +15,7 @@ export default function Home() {
             <span className="-mt-10 font-syne">SOLVR Development</span>
           </div>
           <div className="flex flex-col w-[350px] mt-6 justify-center items-center font-syne text-center">
-            <span className='mb-2'>Comprehensive solutions.</span>Full-service custom websites, landing pages, and web applications.
+            <span className='mb-2'>Comprehensive, full-service solutions.</span>Specializing in custom websites, landing pages, and web applications.
           <button className="cursor-pointer mt-10 px-6 py-3 mx-auto rounded-xl text-[#2c2c2c] font-semibold bg-[#E6E0D6] shadow-[6px_6px_12px_#c2beb6,-6px_-6px_12px_#ffffff] hover:shadow-[inset_4px_4px_8px_#c2beb6,inset_-4px_-4px_8px_#ffffff] transition-all duration-300">
             Contact
           </button>
